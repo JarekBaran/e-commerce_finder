01120060258d7ac36699ad6b7e0e3a03e48ccbcb
Jarek Baran <git@jarekbaran.dev>
2021-02-04 23:41:39 +0100

    Test google search with analyzer

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const google = require("./lib/google");
 const analyzer = require("./lib/analyzer");
 
-const words = [`słuchawki`];
+// const words = [`słuchawki`];
 
 // words.map( word => google( word, 2, 30 )
 //   .then(( data ) => {
-//     console.log( `Dla frazy: ${ data.word } \n` );
-//     console.log( `Sugestie: ${ data.suggestions } \n` );
-//     console.log( `Domeny:` );
+  //     console.log( `Dla frazy: ${ data.word } \n` );
+  //     console.log( `Sugestie: ${ data.suggestions } \n` );
+  //     console.log( `Domeny:` );
 
-//     data.domains.forEach(( domain ) => console.log( domain ));
-//   })
-// );
+  //     data.domains.forEach(( domain ) => console.log( domain ));
+  //   })
+  // );
 
-const urls = ['wordpress-demo.jarekbaran.dev', 'woocommerce-demo.jarekbaran.dev', 'prestashop-demo.jarekbaran.dev', 'devshop-707702.shoparena.pl'];
+  // const urls = ['wordpress-demo.jarekbaran.dev', 'woocommerce-demo.jarekbaran.dev', 'prestashop-demo.jarekbaran.dev', 'devshop-707702.shoparena.pl'];
 
-analyzer(urls)
-  .then((data) => data.forEach((domain) => console.log(domain)));
+  // analyzer(urls)
+  //   .then((data) => data.forEach((domain) => console.log(domain)));
+
+
+  /// TEST
+
+  const words = [`meble dla dzieci 3-6 lat`];
+
+  words.map( word => google( word, 2, 30 )
+    .then(( data ) => {
+      console.log( `Dla frazy: ${ data.word } \n` );
+      console.log( `Sugestie: ${ data.suggestions } \n` );
+      console.log( `Domeny: ${ data.domains.size } \n` );
+
+      data.domains.forEach((domain) => console.log(domain));
+
+      console.log( `\n Analyzer START \n` );
+      analyzer(data.domains)
+        .then((data) => data.forEach((domain) => console.log(domain)));
+  })
+);
